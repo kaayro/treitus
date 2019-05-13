6a417f9613eff8aa2ddf7c54e8e5f10f37f4bf8c
Dalhanlol <50499703+Dalhanlol@users.noreply.github.com>
2019-05-13 16:22:56 -0500

    Update App.js

--- clients/src/App.js
+++ clients/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test number 2
         </a>
       </header>
     </div>
