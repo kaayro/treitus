3e6f9bfd207cbefccfd37708934adb921bdda0b9
Anayeli95 <50536719+Anayeli95@users.noreply.github.com>
2019-05-13 16:31:55 -0500

    Update App.js

--- clients/src/App.js
+++ clients/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pagina de Inicio
+          Pagina de Clientes
         </p>
         <a
           className="App-link"
